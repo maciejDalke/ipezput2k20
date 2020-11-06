d030ca86bc85976549a576b422d6a3a947cf2420
Maciej Dalke <msd93@o2.pl>
2020-11-06 14:55:13 +0100

    add rest repository interface

---
+++ ipezput-2k20-backend/src/main/java/pl/meklad/ipezput2k20/repo/TestDoneRepo.java
@@ -0,0 +1,12 @@
+package pl.meklad.ipezput2k20.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.meklad.ipezput2k20.domain.TestDone;
+
+/**
+ * Create by dev on 24.10.2020
+ */
+@Repository
+public interface TestDoneRepo extends JpaRepository<TestDone,Long> {
+}
---
+++ ipezput-2k20-backend/src/main/java/pl/meklad/ipezput2k20/repo/TestQuestionRepo.java
@@ -0,0 +1,12 @@
+package pl.meklad.ipezput2k20.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.meklad.ipezput2k20.domain.TestQuestions;
+
+/**
+ * Create by dev on 24.10.2020
+ */
+@Repository
+public interface TestQuestionRepo extends JpaRepository<TestQuestions,Long> {
+}
